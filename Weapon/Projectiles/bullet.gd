d1efc95e11a2fbce0d72e92dfe679d27b628837b
Gustvhc <55901045+Gustvhc@users.noreply.github.com>
2025-11-29 05:14:39 -0300

    Projeteis E Movimentação

--- Weapon/Projectiles/bullet.gd
+++ Weapon/Projectiles/bullet.gd
@@ -1,3 +1,14 @@
 extends Projectile
 
 class_name Bullet
+
+@onready var collider = $RayCast3D
+
+var initial_pos: Vector3
+
+func _process(delta: float) -> void:
+	self.position+=transform.basis * Vector3(speed,0,0) * delta
+	if collider.is_colliding():
+		self.queue_free()
+	if(initial_pos.distance_to(self.position) > distance):
+		self.queue_free()
--- Weapon/projectile_spawner.gd
+++ Weapon/projectile_spawner.gd
@@ -2,4 +2,33 @@ extends Marker3D
 
 class_name ProjectileSpawner
 
-@export var projectile: Projectile
+
+#@export var projectile: Node
+@export var character: Node
+
+@export var spawner_downtime:float = 0.0;
+
+var projectile = load("res://Weapon/Projectiles/Bullet.tscn")
+
+var instance
+
+var is_downtime = false
+
+#func _ready():
+	#projectile = projectile.load()
+
+#https://www.youtube.com/watch?v=OdWa6r1yI4U
+#https://www.youtube.com/watch?v=6bbPHsB9TtI
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("Shoot") and not is_downtime:
+		is_downtime = true
+		instance = projectile.instantiate()
+		instance.initial_pos = self.global_position
+		instance.position = self.global_position
+		instance.transform.basis = self.global_transform.basis
+		#get_parent().add_child(instance)
+		character.get_parent().add_child(instance)
+		await get_tree().create_timer(1.0).timeout
+
+func _on_timer_timeout() -> void:
+	is_downtime = false
